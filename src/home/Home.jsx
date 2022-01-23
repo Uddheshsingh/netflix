e3d912528b9ae4ea7a36ed9f101d6e19f6aeecc4
Uddhesh1 <89867268+Uddhesh1@users.noreply.github.com>
2022-01-24 04:49:05 +0530

    Initial commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,19 @@
+
+import Navbar from "../components/navbar/Navbar";
+import "./home.scss"
+
+const Home = () => {
+  return (
+  <div className="home">
+      <Navbar/>
+      <img 
+      width="100%"
+      src="https://www.thesun.co.uk/wp-content/uploads/2020/10/NINTCHDBPICT000615305526.jpg"
+      alt=""
+      />
+  </div>
+  )
+};
+
+export default Home;
+
